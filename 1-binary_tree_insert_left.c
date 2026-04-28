7c5f59a2e73a8d27ca640c45ba5f49a0e2897561
Mayamner <mem.kpope@gmail.com>
2026-04-28 20:23:02 +0300

    Second Task summition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left - inserts a node as the left child
+ * @parent: pointer to the parent node
+ * @value: value to store in the new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
