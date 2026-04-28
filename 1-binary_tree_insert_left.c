59cc1aa09a95e3be99bf34de279ece7b62446e3b
Mayamner <mem.kpope@gmail.com>
2026-04-28 20:11:20 +0300

    Second Task summition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,31 +3,31 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = value;
-    new_node->parent = parent;
+	new_node->n = value;
+	new_node->parent = parent;
 
-    /* If parent already has a left child */
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    }
-    else
-    {
-        new_node->left = NULL;
-    }
+	/* If parent already has a left child */
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	else
+	{
+		new_node->left = NULL;
+	}
 
-    new_node->right = NULL;
-    parent->left = new_node;
+	new_node->right = NULL;
+	parent->left = new_node;
 
-    return (new_node);
+	return (new_node);
 }
