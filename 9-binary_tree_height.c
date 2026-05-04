c85c454ebcff8557a831db75b7a1f83a33d71d2a
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-05-04 08:06:50 +0000

    Fixed binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,17 +11,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_h = 0;
 	size_t right_h = 0;
 
-	/* If the node is NULL, return 0 as per requirements */
 	if (tree == NULL)
 		return (0);
 
-	/* * Calculate height of the left subtree:
-	 * If left child exists, height is 1 + height of the child.
+	/*
+	 * If the left child exists, height is 1 + height of left subtree.
+	 * Otherwise, left_h remains 0.
 	 */
 	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 
-	/* * Calculate height of the right subtree:
-	 * If right child exists, height is 1 + height of the child.
+	/*
+	 * If the right child exists, height is 1 + height of right subtree.
+	 * Otherwise, right_h remains 0.
 	 */
 	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
